9bb830237a605ae9fa81df065f480e8ac7752722
nickmerl <frankmerlino@Franks-MacBook-Pro.local>
2019-10-31 14:03:50 -0400

    test

--- ui.R
+++ ui.R
@@ -2,5 +2,6 @@ fluidPage(
   style = "padding-top: 150px; background-color: orange; height: 100vh;",
   class = "text-center",
   h1("Happy Halloween"),
-  h2("Booo!!")
+  h2("Booo!!"),
+  p("testing git")
 )
